3e34adab0ca986f04ab70957843445698a5c893b
Devilsbkssks <119072774+Devilsbkssks@users.noreply.github.com>
2022-12-29 15:30:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
     API_HASH = os.getenv("API_HASH", "016d5e115a06ddfb6121823d72ae4d8c")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "5918078029:AAHOlk_opieJ0cgVOMxOHdoHufraiaYkZHU")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "Mdiskwalabot")
-    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOIABuxG4tmdSPqIRA916mSFE2SUI4q2rbLgkdxjPaFCquOvUDwBvDGDimxS-hL5y4B851tjjm-6PkjurqmR3G2iYBwGOwLD5nt6987VaG0MDZWFg9Eth-7TIFyfQjm6NWdwimdOLXAXlLheXS33gPrtJe1mIbw1SEk1dkuz03jVy-ZIChHTmRJHC3nvzikocdFbNb2PxhPCAqhFkG4M5LyNw4LDklmRZkB20mfJ6SXmrIs5K5PQ0ZyguUQNzCo-UvWeFJFNl1iaM0FXdypg-BtgVBhwoQE-ldgqgNPDH8U96apy6gEhGo3PHGikxhGgg_kyPc90VbGcel_bHcpc6btANzUY=")
+    USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOJIBuz4zHjqCaL7w5pqreizPQhvde3p4LfJMmGqr2h8NcyfEuZlp2U7HGUkuBujYH1TBNRUNg8olHmbxRtZ3b14TaBsGMEekmoFq1Cs0m4QkiWt0e-Itl5wDHMSXbpIFN9zV0d9rmG9lj6Yiubx0hOlC36RuWrbFoyRrbdIswfhYqnIhXbsjbayaYyYtLH8fK14hjDhaU5a4xRuNEy-hHdDT60r-7smLc3qwNOYReZd4-tnsElQCHKbblvEE4ZK9ui5KnCw1WNPHU3DZ0adMUrjmzHqEc6WOI7X3cFhrL3O0e0A3qdwqmXJa4CDvm4_wXBbffUm8uSshXM6p7xsVSZFoM90=")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001885587345)) 
     BOT_USERNAME = os.getenv("BOT_USERNAME", "Mdisk_search_re_bot")
     BOT_OWNER = int(os.getenv("BOT_OWNER", "5104293442"))
